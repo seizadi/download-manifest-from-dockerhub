133e50756b200133513dfc2daea4b7e06c98e958
Soheil Eizadi <seizadi@infoblox.com>
2017-07-31 15:44:51 -0700

    Added support for Auth for Private Repo Pulls

--- download-manifest.py
+++ download-manifest.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 import sys
 import json
 
 import requests
+import argparse
 
 
 login_template = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:{repository}:pull"
@@ -13,12 +14,16 @@ def pretty_print(d):
     print(json.dumps(d, indent=2))
 
 
-def download_manifest_for_repo(repo, tag):
+def download_manifest_for_repo(repo, tag, username, password):
     """
     repo: string, repository (e.g. 'library/fedora')
     tag:  string, tag of the repository (e.g. 'latest')
     """
-    response = requests.get(login_template.format(repository=repo), json=True)
+    if not username:
+        response = requests.get(login_template.format(repository=repo), json=True)
+    else:
+        response = requests.get(login_template.format(repository=repo), json=True, auth=(username, password))
+
     response_json = response.json()
     token = response_json["token"]
     response = requests.get(
@@ -33,11 +38,18 @@ def download_manifest_for_repo(repo, tag):
 
 
 def main():
-    repos = sys.argv[1:]
+    parser = argparse.ArgumentParser(description='Get Docker Repositry Manifest')
+    parser.add_argument('repos', metavar='Repos', nargs='+', help='Registery e.g. library/fedora')
+    parser.add_argument('--username', '-u', metavar='USERNAME')
+    parser.add_argument('--password', '-p', metavar='PASSWORD')
+
+    # repos = sys.argv[1:]
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
+    repos = args.repos
     if not repos:
-        print("Usage: {} <[namespace/]repository[:tag]> [<[namespace/]repository[:tag]>...]".format(sys.argv[0]) +
-              "\nExample: {} fedora:23".format(sys.argv[0]))
-        return 1
+        return 1 # parser will not allow us to get here!
     for repo_tag in repos:
         if ":" in repo_tag:
             repo, tag = repo_tag.split(":")
@@ -45,7 +57,7 @@ def main():
             repo, tag = repo_tag, "latest"
         if "/" not in repo:
             repo = "library/" + repo
-        pretty_print(download_manifest_for_repo(repo, tag))
+        pretty_print(download_manifest_for_repo(repo, tag, username, password))
         return 0
 
 
